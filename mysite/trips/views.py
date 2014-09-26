72413ba6b25bee1ef32ad2c5aee64b5a16dad169
Andy Dai <daikeren@gmail.com>
2014-09-26 15:06:37 +0800

    Complete Chapter 5. Templates

--- mysite/trips/views.py
+++ mysite/trips/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
 
 def hello_world(request):
-    return HttpResponse("Hello World!")
+    return render(request,
+                  'hello_world.html',
+                  {'current_time': datetime.now()})
