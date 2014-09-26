733e712a75a228d8f29a2ad5c0ebdf1173564584
Andy Dai <daikeren@gmail.com>
2014-09-26 15:28:15 +0800

    Complete Chapter 11. Deploy

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -86,3 +86,28 @@ STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates').replace('\\', '/'),
 )
+
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+
+# Turn off DEBUG mode
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+# Import all of local settings if the file exists
+try:
+    from .local_settings import *
+except ImportError:
+    pass
