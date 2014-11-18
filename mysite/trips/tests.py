059354439e752c2e68d18d93450ba00025e69de3
Andy Dai <daikeren@gmail.com>
2014-11-18 16:04:58 +0800

    Updated .gitignore

--- mysite/trips/tests.py
+++ mysite/trips/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class SimpleTest(TestCase):
+    def testURLStatus(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
